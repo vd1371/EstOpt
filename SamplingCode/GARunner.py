4d026e050d981b5c9b1236f789be9af15d1b1d3a
Vahid <vd1371@gmail.com>
2021-07-08 17:29:58 +0800

    sharing the load on processes

--- SamplingCode/GARunner.py
+++ SamplingCode/GARunner.py
@@ -1,26 +1,43 @@
 import os
+import multiprocessing as mp
 from subprocess import call, Popen, PIPE, STDOUT
 import json
 from _utils import load_indiana_assets
 
 
-p = Popen("powershell python GAOpt.py", stdout=PIPE, stdin = PIPE)
+def run_indiana():
+
+	n_cores = mp.cpu_count() - 2
+
+	assets = load_indiana_assets()
+	ids = list(assets.keys())
+	sect_len = int(len(assets) / n_cores) + 1
+
+	for i in range(n_cores):
+
+		process_assets = {}
+
+		final_index = min((i+1)*sect_len, len(assets))
+		init_index = i*sect_len
+
+		for k in ids[init_index : final_index]:
+			process_assets[k] = assets[k]
 
 
-assets = load_indiana_assets(4)
+		p = Popen("powershell python GAOpt.py", stdout=PIPE, stdin = PIPE)
+		response = p.communicate(json.dumps(process_assets).encode('utf-8'))
+		print (response)
+
+		p.stdout.close()
 
 
-response = p.communicate(json.dumps(assets).encode('utf-8'))
-
-print (response)
-
-p.stdout.close()
-
-
-print ('Done')
+	print ('Done')
 
 
 
 # for i in range (N):
 # 	print (i, 'is called')
 # 	Popen("powershell python GAOpt.py")
+
+if __name__ == "__main__":
+	run_indiana()
